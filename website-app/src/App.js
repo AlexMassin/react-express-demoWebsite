87e0aaa5e315d4c723f93508855c860d7f43683c
Alexander Philip Tarek Massin <aptmassin@gmail.com>
2018-10-19 14:06:42 -0400

    Added semantic-css and fixed coming soon page

--- website-app/src/App.js
+++ website-app/src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
+import {Header, Transition, Image, Icon} from 'semantic-ui-react';
 import './App.css';
+import { unstable_getThreadID } from 'schedule/cjs/schedule-tracing.development';
 
 class App extends Component {
+  state={visible:false}
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  componentDidMount = () => {
+      setTimeout(
+        this.setState({visible:true}),
+        2000
+      )
+  }
+
   render() {
+    const {visible} = this.state
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Coming Soon!
-          </p>
+          <Image src={logo} className="App-logo" alt="logo"/>
+          <Transition visible={visible} animation='fly left' duration={3000}>
+              <Header as='h1' icon={<Icon name='truck' flipped='horizontally'/>} content='Coming Soon' color={'yellow'} />
+          </Transition>
         </header>
       </div>
     );
