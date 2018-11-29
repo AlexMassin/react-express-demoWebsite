04fdbea70233c36292b1afdd52cdcbcef6070d50
Alexander Philip Tarek Massin <aptmassin@gmail.com>
2018-11-29 18:08:49 -0500

    Navbar fixes and footer updated

--- website-app/src/components/Footer/Footer.js
+++ website-app/src/components/Footer/Footer.js
@@ -20,41 +20,11 @@ class Footer extends Component {
 
   render() {    
     return (
-      <div>
-  <Segment inverted vertical style={{ padding: '5em 0em' }}>
-<Container>
-  <Grid divided inverted stackable>
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Header inverted as='h4' content='About' />
-        <List link inverted>
-          <List.Item as='a'>Sitemap</List.Item>
-          <List.Item as='a'>Contact Us</List.Item>
-          <List.Item as='a'>Religious Ceremonies</List.Item>
-          <List.Item as='a'>Gazebo Plans</List.Item>
-        </List>
-      </Grid.Column>
-      <Grid.Column width={3}>
-        <Header inverted as='h4' content='Services' />
-        <List link inverted>
-          <List.Item as='a'>Banana Pre-Order</List.Item>
-          <List.Item as='a'>DNA FAQ</List.Item>
-          <List.Item as='a'>How To Access</List.Item>
-          <List.Item as='a'>Favorite X-Men</List.Item>
-        </List>
-      </Grid.Column>
-      <Grid.Column width={7}>
-        <Header as='h4' inverted>
-          Footer Header
-        </Header>
-        <p>
-          Extra space for a call to action inside the footer that could help re-engage users.
-        </p>
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-</Container>
-</Segment>
+      <div style={{margin: '14px'}}>
+      <center>
+            <Icon size='large' name='hand point down outline' />
+            This is the bottom of the page.
+      </center>
     </div>
     );
   }
--- website-app/src/components/NavBar/NavBar.js
+++ website-app/src/components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import {
   Container,
   Menu,
   Icon,
+  Button
 } from 'semantic-ui-react'
 
 class NavBar extends Component {
@@ -22,7 +23,7 @@ class NavBar extends Component {
         <Menu
           fixed='top'
           inverted
-          style={{opacity: '0.9'}}
+          style={{opacity: '0.9', height:'2em'}}
           color='violet'
           size='large'
           secondary
@@ -46,9 +47,17 @@ class NavBar extends Component {
               <Menu.Item  name='team' 
                         active={window.location.pathname === '/team'} 
                         onClick={this.handleItemClick} />
+            {Boolean(window.location.pathname === '/demonstration') && 
             <Menu.Item position='right'>
-              <Icon name='react' size='large' />
-            </Menu.Item>
+              <Button basic color='yellow' id='shopping-cart'  animated='fade in'>
+              <Button.Content hidden>
+                <Icon size='large' style={{marginTop: '-4px'}} name='in cart' />
+              </Button.Content>
+              <Button.Content visible>
+                <Icon size='large' name='cart' />
+              </Button.Content>
+              </Button>
+            </Menu.Item>}
           </Container>
         </Menu>
     {children}
