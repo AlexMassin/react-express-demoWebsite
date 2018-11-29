6749f0cc2ea4a242b7b4bfcf58499ab9e7730716
Alex Gomes <glexaomes@gmail.com>
2018-11-29 17:48:56 -0500

    Demonstation Work
    Craeted HatCard component for shop items. Added images for each hat. Components take in parameters. Components also have a button that will add items to cart. Cart not yet implemented.

--- website-app/src/pages/Demonstration/Demonstration.js
+++ website-app/src/pages/Demonstration/Demonstration.js
@@ -1,142 +1,163 @@
 import 'semantic-ui-css/semantic.min.css';
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Button,
-  Container,
   Divider,
   Grid,
   Header,
   Image,
   List,
   Segment,
+  Accordion,
+	Icon,
+	Popup,
+	Container,
+	Transition,
+	Radio,
+  GridRow,
+  Card,
+  CardGroup
 } from 'semantic-ui-react'
+import HatCard from '../../components/HatCard/HatCard';
+import hat_TR_NBA from "../../images/hat_TR_NBA.jpeg";
+import hat_MH_NBA from "../../images/hat_MH_NBA.jpeg";
+import hat_CB_NBA from "../../images/hat_CB_NBA.jpeg";
+import hat_LAL_NBA from "../../images/hat_LAL_NBA.jpeg";
+import hat_BJ_MLB from "../../images/hat_BJ_MLB.jpeg";
+import hat_NYY_MLB from "../../images/hat_NYY_MLB.jpeg";
+import hat_DT_MLB from "../../images/hat_DT_MLB.jpeg";
+import hat_BRS_MLB from "../../images/hat_BRS_MLB.jpeg";
+import hat_TML_NHL from "../../images/hat_TML_NHL.jpeg";
+import hat_EO_NHL from "../../images/hat_EO_NHL.jpeg";
+import hat_CAN1_NHL from "../../images/hat_CAN1_NHL.jpeg";
+import hat_CAN2_NHL from "../../images/hat_CAN2_NHL.jpeg";
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
  * such things.
  */
 
-const Demonstration = () => (
-  <div>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Grid container stackable verticalAlign='middle'>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              We can give your company superpowers to do things that they never thought possible.
-              Let us delight your customers and empower your needs... through pure data analytics.
-            </p>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-              bioengineered.
-            </p>
-          </Grid.Column>
-          <Grid.Column floated='right' width={6}>
-            <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign='center'>
-            <Button size='huge'>Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+class Demonstration extends Component {
+
+  state={visible:false}
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+
+componentDidMount = () => {
+    setTimeout(
+      this.setState({visible:true}),
+      3000
+    )
+}
+
+render() {
+  const {visible} = this.state
+  return (
+    <div>
+      <Segment inverted style={{ padding: '8em 0em' }} vertical>
+        <Transition visible={visible} animation="fade down" duration={4000}>
+				  <Container textAlign="center">
+						<Header
+							style={{ fontSize: '72px', color: '#FC8200' }}
+							as="h1"
+							inverted
+							content={'Imperial Caps'}
+						/>
+						<Header style={{ fontSize: '20px' }} as="h3" inverted color="white">
+							Modern Day Helmets for Modern Day Warriors <Icon name='hand rock outline' style={{ color: '#FC8200' }}/>
+					</Header>
+				</Container>
+			</Transition>
     </Segment>
-    <Segment style={{ padding: '0em' }} vertical>
-      <Grid celled='internally' columns='equal' stackable>
-        <Grid.Row textAlign='center'>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "What a Company"
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='/images/avatar/large/nan.jpg' />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Instead of focusing on content creation and hard work, we have learned how to master the
-          art of doing nothing by providing massive amounts of whitespace and generic content that
-          can seem massive, monolithic and worth your attention.
-        </p>
-        <Button as='a' size='large'>
-          Read More
-        </Button>
-        <Divider
-          as='h4'
-          className='header'
-          horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-        >
-          <a href='#'>Case Studies</a>
-        </Divider>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: '1.33em' }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-          it's really true. It took years of gene splicing and combinatory DNA research, but our
-          bananas can really dance.
-        </p>
-        <Button as='a' size='large'>
-          I'm Still Quite Interested
-        </Button>
-      </Container>
-    </Segment>
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
-  </div>
-)
+
+    <Card.Group itemsPerRow={4} centered style={ {margin: '1em 0em' }}>
+      <HatCard 
+        imgsrc={hat_TR_NBA} 
+        name='Toronto Raptor New Era NBA City Series 2.0'
+        sub='59FIFTY Cap'
+        price='$48.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_MH_NBA}
+        name='Miami Heat NBA City Series 2.0'
+        sub='59FIFTY Cap'
+        price='$48.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_CB_NBA}
+        name='Chicago Bulls NBA City Series 2.0'
+        sub='59FIFTY Cap'
+        price='$48.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_LAL_NBA}
+        name='Los Angeles Lakers NBA City Series 2.0'
+        sub='59FIFTY Cap'
+        price='$48.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_BJ_MLB} 
+        name='Toronto Blue Jays New Era MLB Authenic Collection'
+        sub='59FIFTY Cap'
+        price='$42.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_NYY_MLB} 
+        name='New York Yankees New Era MLB Authenic Collection'
+        sub='59FIFTY Cap'
+        price='$42.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_DT_MLB} 
+        name='Detroit Tigers New Era MLB Authenic Collection'
+        sub='59FIFTY Cap'
+        price='$42.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_BRS_MLB} 
+        name='Boston Red Sox New Era MLB Authenic Collection'
+        sub='59FIFTY Cap'
+        price='$42.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_TML_NHL} 
+        name='Toronto Maple Leafs NHL Rinkside'
+        sub='Flex Cap'
+        price='$40.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_EO_NHL} 
+        name='Edmonton Oilers NHL Throwback'
+        sub='Snapback Cap'
+        price='$39.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_CAN1_NHL} 
+        name='Canada Hockey Nike IIHF Dri-Fit Wool'
+        sub='Adjustable Cap'
+        price='$29.99'
+        />
+
+        <HatCard 
+        imgsrc={hat_CAN2_NHL} 
+        name='Canada Hockey Nike Olympics Dri-Fit Wool'
+        sub='Classic Cap'
+        price='$29.99'
+        />
+      </Card.Group>
+    </div>
+  );
+}
+}
 export default Demonstration;
